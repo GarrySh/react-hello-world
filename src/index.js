912fa6ec341ca8182e5b7bec885e72d0cb8af159
GarrySh <sooo.lame@gmail.com>
2018-09-25 20:28:54 +1000

    add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import App from './App';
 
-ReactDom.render(
-  <App />,
-  document.getElementById('root')
-);
+// ReactDom.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+import { createStore } from 'redux';
+
+const playlist = (state = { tracks: [] }, action) => {
+  switch (action.type) {
+    case 'ADD_TRACK': {
+      const { tracks } = state;
+      return { tracks: [...tracks, action.payload] };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const store = createStore(playlist);
+
+store.subscribe(() => {
+  console.log('subscribe', store.getState())
+});
+
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
