2a7ac3d253af3b8925a540d7c5434ed2c32c8f91
GarrySh <sooo.lame@gmail.com>
2018-10-12 15:33:52 +1000

    react-router

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import App from './App';
+import Dropdown from './Dropdown';
 import reducers from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import Menu from './Menu';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
+const newRoute = () => <div>New route</div>
+const Error = () => <div>Error</div>
+
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Menu />
+        <Switch>
+          <Route path="/" component={App} exact />
+          <Route path="/dropdown" component={Dropdown} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
